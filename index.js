df63421008f2830f67a7537dc0f25622da0f110c
Mada-Z <64742082+Mada-Z@users.noreply.github.com>
2022-01-12 18:46:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const { createServer } = require("http");
+const ws = require("ws");
+const { useServer } = require("graphql-ws/lib/use/ws");
+const { execute, subscribe } = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
 const authorizationMiddleware = require('./middlewares/authorization');
 
@@ -6,6 +10,10 @@ const port = 3001;
 const app = express();
 
 const schema = require("./graphql");
+
+const server = createServer(app);
+
+require("./pubsub");
 
 app.use(
   "/graphql",
@@ -16,6 +24,16 @@ app.use(
   })
 );
 
-app.listen(port, () => {
+const wsServer = new ws.Server({
+  server,
+  path: '/subscriptions'
+});
+
+server.listen(port, () => {
+  useServer({
+    schema,
+    execute,
+    subscribe,
+  }, wsServer);
   console.log("Server started on", port);
 });
